8a12d2ac07738589451d8ecbaccbf8dd7e46c5b8
Fabio <pulfabio@hotmail.com>
2017-02-01 12:56:30 +0100

    get geolocation data through ip address to avoid cross origin issues in codepen.io

--- js/main.js
+++ js/main.js
@@ -1,19 +1,11 @@
 //Helper function that consumes weather APIs
-function getWeatherData(lat, lon, celsius) {
-    //Temperature units
-    var units = "";
-    if (celsius) {
-        units = "metric";
-    } else {
-        units = "imperial";
-    };
-
+function getWeatherData(lat, lon) {
     //API key
     var key = "5bef4f9d1bda594aa4e096338d5f39d1";
 
     //API url
     apiUrl = "http://api.openweathermap.org/data/2.5/weather?";
-    apiUrl += "lat=" + lat + "&lon=" + lon + "&units=" + units +
+    apiUrl += "lat=" + lat + "&lon=" + lon + "&units=metric" +
         "&APPID=" + key; // + "&callback=test";
 
     //API request
@@ -26,7 +18,8 @@ function getWeatherData(lat, lon, celsius) {
 
             $(".location").html(json["name"]);
 
-            $(".temperature").html(Math.floor(json["main"]["temp"]));
+            temp = Math.floor(json["main"]["temp"])
+            $(".temperature").html(temp);
 
             $(".weather").html(json["weather"][0]["description"]);
 
@@ -38,42 +31,49 @@ function getWeatherData(lat, lon, celsius) {
     });
 }
 
-//Helper function that displays selected temperature metric
-function applyUnits(celsius) {
+//Helper function that displays temperature in selected metric
+function applyUnits(celsius, temp) {
     if (celsius) {
         $(".units").html("C");
+        $(".temperature").html(temp);
     } else {
         $(".units").html("F");
+        $(".temperature").html(Math.floor((temp * 9 / 5) + 32));
     }
 }
 
-function setWeather(celsius) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        lat = position.coords.latitude;
-        lon = position.coords.longitude;
-        getWeatherData(lat, lon, celsius);
-    });
+function setLocAndWeather() {
+    $.get("http://ipinfo.io", function(response) {
+        var location = response.loc.split(",");
+        lat = location[0];
+        lon = location[1];
+        country = response.country;
+        getWeatherData(lat, lon);
+    }, "jsonp")
 }
 
+//Declaring global variables
+var temp = 0;
+var lat = "";
+var lon = "";
+var country = "";
+var celsius = true;
+
 $(document).ready(function() {
-    //Initialize units and position
-    var celsius = true;
-    var lat = "";
-    var lon = "";
+    //Set location and get weather
+    setLocAndWeather();
+    console.log(temp);
 
     //Initialize units display
-    applyUnits(celsius);
+    applyUnits(celsius, temp);
 
-    //Get Weather data
-    setWeather(celsius);
-
-    //Units button functionality
+    //Add units button functionality
     $(".units").on("click", function(){
         celsius = !celsius;
-        applyUnits(celsius);
-        setWeather(celsius);
+        applyUnits(celsius, temp);
     });
 
+    //Change button style on mouse over
     $(".units").hover(
         function() {
             $(this).css("color", "#0000b3")
@@ -85,11 +85,19 @@ $(document).ready(function() {
     );
 });
 
-/*
+/* OBSOLETE!
 function getPosition() {
     navigator.geolocation.getCurrentPosition(function(position) {
         lat = position.coords.latitude;
         lon = position.coords.longitude;
+    });
+}
+
+function setWeather(celsius) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        lat = position.coords.latitude;
+        lon = position.coords.longitude;
+        getWeatherData(lat, lon, celsius);
     });
 }
 */
